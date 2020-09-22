697266d9bb4d61f676fecd9928456f6f2869ddad
Van Huy Pham <vanhuycx@gmail.com>
2020-09-21 20:02:02 -0400

    update changes

--- buy-and-sell-back-end/src/routes/getListing.js
+++ buy-and-sell-back-end/src/routes/getListing.js
@@ -7,7 +7,7 @@ export const getListingRoute = {
   handler: (req, h) => {
     const id = req.params.id;
     const listing = fakeListings.find((listing) => listing.id === id);
-    if (!listing) throw Boom.notFound(`Listing does not exist: id ${id}`);
+    if (!listing) throw Boom.notFound(`Listing does not exist with id ${id}`);
     return listing;
   },
 };
